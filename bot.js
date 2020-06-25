0ddc894f3b699375e0293c9fb65d0e653d4db65f
dosclan <37374004+dosclan@users.noreply.github.com>
2020-06-25 22:06:37 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
+
+
+client.login(process.env.BOT_TOKEN);
